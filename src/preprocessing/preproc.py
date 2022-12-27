6c65f135b4c51914cef854e8403ca9a95e7d98da
Lorena Calvo Bartolomé <100330717@alumnos.uc3m.es>
2022-12-27 09:42:53 +0100

    Update preproc to pd.

--- src/preprocessing/preproc.py
+++ src/preprocessing/preproc.py
@@ -20,8 +20,8 @@ from dask.diagnostics import ProgressBar
 import logging
 from preproc_utils import check_nltk_packages
 
-#!python -m spacy download en_core_web_lg
-#!python -m spacy download xx_sent_ud_sm
+#!python3 -m spacy download en_core_web_lg
+#!python3 -m spacy download xx_sent_ud_sm
 #!pip install --upgrade spacy_langdetect
 
 class nlpPipeline():
@@ -184,30 +184,42 @@ class nlpPipeline():
         self.nlp.disable_pipe('ner')
         
         # Lemmatize text
-        corpus_df['lemmas'] = corpus_df["raw_text"].apply(self.do_pipeline, meta=('lemmas', 'object'))
+        #corpus_df['lemmas'] = corpus_df["raw_text"].apply(self.do_pipeline, meta=('lemmas', 'object'))
+        corpus_df['lemmas'] = corpus_df["raw_text"].apply(self.do_pipeline)
         
         # Create corpus from tokenized lemmas
-        corpus = corpus_df['lemmas'].compute().values
+        #dbag = corpus_df['lemmas'].to_dask_array().compute()
+        #dbag_list = list(dbag)
+        #print(dbag)
+        #print(len(dbag_list))
         
-        # Split into words
-        corpus = [el.split() for el in corpus]
+        #corpus = corpus_df['lemmas'].compute().values
+        corpus = corpus_df['lemmas'].values
+        print(len(corpus))
+        print(len(corpus_df))
+
+        corpus2 = [el.split() for el in corpus]
         
         # Create Phrase model for n-grams detection
-        phrase_model = Phrases(corpus, min_count=2, threshold=20, connector_words=ENGLISH_CONNECTOR_WORDS)
+        phrase_model = Phrases(corpus2, min_count=2, threshold=20)
         
-         # Carry out n-grams substitution
-        corpus = [el for el in phrase_model[corpus]] 
+        # Carry out n-grams substitution
+        corpus2 = [el for el in phrase_model[corpus2]] 
 
-        corpus = [" ".join(el) for el in corpus]
+        corpus3 = [" ".join(el) for el in corpus2]
+
+        print(len(corpus3))
+        #print(len(corpus_df.compute()))
 
         def get_ngram(row):
-            return corpus.pop(0)
+            return corpus3.pop(0)
 
         # Save n-grams in new column in the dataFrame
-        corpus_df["lemmas_with_grams"] =  corpus_df.apply(get_ngram, meta=('lemmas_with_grams', 'object'), axis=1)
+        #corpus_df["lemmas_with_grams"] =  corpus_df.apply(get_ngram, meta=('lemmas_with_grams', 'object'), axis=1)
+        corpus_df["lemmas_with_grams"] =  corpus_df.apply(get_ngram, axis=1)
         
         return corpus_df
-            
+                
     
 if __name__ == "__main__":
 
@@ -246,24 +258,27 @@ if __name__ == "__main__":
         if args.source == "cordis":
             logger.info(
                 f'-- -- Reading from Cordis...')
-            raw_text_fld = "objective"#objective
+            raw_text_fld = "summary"#objective
             title_fld = "title"
             
         df = pd.read_excel(source_path)
-        corpus_df = dd.from_pandas(df, npartitions=3)
+        #corpus_df = dd.from_pandas(df, npartitions=3)
         
-        #corpus_df = corpus_df.sample(frac=0.001, replace=True, random_state=1)
+        corpus_df = df.sample(frac=0.001, replace=True, random_state=1)
         corpus_df = corpus_df[[raw_text_fld, title_fld]]
         
         # Detect abstracts' language and filter out those non-English ones
-        corpus_df['langue'] = corpus_df[raw_text_fld].apply(det, meta=('langue', 'object'))
+        #corpus_df['langue'] = corpus_df[raw_text_fld].apply(det, meta=('langue', 'object'))
+        corpus_df['langue'] = corpus_df[raw_text_fld].apply(det)
+        
         corpus_df = corpus_df[corpus_df['langue'] == 'en']
         
         # Filter out abstracts with no text   
         corpus_df = corpus_df[corpus_df[raw_text_fld] != ""]
         
         # Concatenate title + abstract/summary
-        corpus_df["raw_text"] = corpus_df[[title_fld, raw_text_fld]].apply(" ".join, axis=1, meta=('raw_text', 'object'))
+        #corpus_df["raw_text"] = corpus_df[[title_fld, raw_text_fld]].apply(" ".join, axis=1, meta=('raw_text', 'object'))
+        corpus_df["raw_text"] = corpus_df[[title_fld, raw_text_fld]].apply(" ".join, axis=1)
         
     elif args.source_type == "parquet":
         if args.source == "scholar":
@@ -287,14 +302,16 @@ if __name__ == "__main__":
             df = df[df[raw_text_fld] != ""]
             
             # Detect abstracts' language and filter out those non-English ones
-            df['langue'] = df[raw_text_fld].apply(det, meta=('langue', 'object'))
+            #df['langue'] = df[raw_text_fld].apply(det, meta=('langue', 'object'))
+            df['langue'] = df[raw_text_fld].apply(det)
             df = df[df['langue'] == 'en']
             
             # Filter out abstracts with no text   
             df = df[df[raw_text_fld] != ""]
             
             # Concatenate title + abstract/summary
-            df["raw_text"] = df[[title_fld, raw_text_fld]].apply(" ".join, axis=1, meta=('raw_text', 'object'))
+            #df["raw_text"] = df[[title_fld, raw_text_fld]].apply(" ".join, axis=1, meta=('raw_text', 'object'))
+            df["raw_text"] = df[[title_fld, raw_text_fld]].apply(" ".join, axis=1)
             
             # Concatenate dataframes
             if idx == 0:
@@ -304,7 +321,7 @@ if __name__ == "__main__":
     
     # Get stopword lists
     stw_lsts = []
-    for entry in pathlib.Path("/home/lbartolome/hierarchical-topic-models/data/stw_lists").iterdir():
+    for entry in pathlib.Path("/workspaces/hierarchical-topic-models/data/stw_lists").iterdir():
         #/home/lbartolome/hierarchical-topic-models/data/stw_lists
         # check if it is a file
         if entry.as_posix().endswith("txt"):
@@ -317,13 +334,15 @@ if __name__ == "__main__":
     
     corpus_df = nlpPipeline.preproc(corpus_df)
     
-    print(corpus_df.compute())
+    print(corpus_df)
             
     # Save new df in parquet file
     outFile = destination_path.joinpath("preproc_" + args.source + ".parquet")
     if outFile.is_file():
         outFile.unlink()
-
+    corpus_df.to_parquet(outFile.as_posix())
+    
+    """
     with ProgressBar():
         if args.nw > 0:
             corpus_df.to_parquet(outFile, write_index=False, schema="infer", compute_kwargs={
@@ -332,6 +351,7 @@ if __name__ == "__main__":
             # Use Dask default number of workers (i.e., number of cores)
             corpus_df.to_parquet(outFile, write_index=False, schema="infer", compute_kwargs={
                 'scheduler': 'processes'})
+    """
 
 
     
--- src/preprocessing/trail.py
+++ src/preprocessing/trail.py
@@ -3,4 +3,4 @@ import pandas as pd
 
 df2 = pd.read_parquet("/workspaces/hierarchical-topic-models/data/preproc/preproc_cordis.parquet")
 
-print(df2.columns)
+print(df2)

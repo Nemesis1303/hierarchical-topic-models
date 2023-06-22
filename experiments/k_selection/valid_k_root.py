43c2dfebadea00a6b056cf506acdb178a310b4e9
Lorena Calvo Bartolomé <100330717@alumnos.uc3m.es>
2023-06-22 23:29:23 +0200

    Removed break.

--- experiments/k_selection/valid_k_root.py
+++ experiments/k_selection/valid_k_root.py
@@ -176,9 +176,6 @@ def main():
             )
     training_params = read_config_experiments(config_file)
     
-    print(training_params)
-    import pdb; pdb.set_trace()
-
     # grid_params = [
     #    [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150],
     #    [0.1, 0.5, 1, 5, 10, 20, 50],

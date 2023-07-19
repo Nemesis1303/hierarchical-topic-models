86fbce41f81136eb7a87e2a1bb7cbc64def216db
Lorena Calvo Bartolomé <100330717@alumnos.uc3m.es>
2023-07-19 15:25:53 +0200

    Update imports.

--- src/evaluateNN/OCTIS/octis/__init__.py
+++ src/evaluateNN/OCTIS/octis/__init__.py
@@ -1,11 +1,11 @@
 """Top-level package for OCTIS."""
-from OCTIS.octis.dashboard import *
-from OCTIS.octis.configuration import *
-from OCTIS.octis.dataset import *
-from OCTIS.octis.evaluation_metrics import *
-from OCTIS.octis.models import *
-from OCTIS.octis.optimization import *
-from OCTIS.octis.preprocessing import *
+from src.evaluateNN.OCTIS.octis.dashboard import *
+from src.evaluateNN.OCTIS.octis.configuration import *
+from src.evaluateNN.OCTIS.octis.dataset import *
+from src.evaluateNN.OCTIS.octis.evaluation_metrics import *
+from src.evaluateNN.OCTIS.octis.models import *
+from src.evaluateNN.OCTIS.octis.optimization import *
+from src.evaluateNN.OCTIS.octis.preprocessing import *
 
 __author__ = """Silvia Terragni"""
 __email__ = 's.terragni4@campus.unimib.it'

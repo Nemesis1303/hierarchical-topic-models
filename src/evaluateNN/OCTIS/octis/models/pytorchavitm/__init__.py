ef2d83301ad1c2b9ce72d85e0e99cb15a980be77
Lorena Calvo Bartolomé <100330717@alumnos.uc3m.es>
2023-07-19 15:35:07 +0200

    Updated imports.

--- src/evaluateNN/OCTIS/octis/models/pytorchavitm/__init__.py
+++ src/evaluateNN/OCTIS/octis/models/pytorchavitm/__init__.py
@@ -1,3 +1,3 @@
 """Init package"""
 
-from OCTIS.octis.models.pytorchavitm.avitm.avitm_model import AVITM_model
+from src.evaluateNN.OCTIS.octis.models.pytorchavitm.avitm.avitm_model import AVITM_model

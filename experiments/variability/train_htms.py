9c724a6a22a9b5d8648cef5b713fb8f03f24271c
Lorena Calvo Bartolomé <100330717@alumnos.uc3m.es>
2023-08-21 15:38:48 +0200

    Updated notebooks experiments.

--- experiments/variability/train_htms.py
+++ experiments/variability/train_htms.py
@@ -57,7 +57,7 @@ def train_automatic(path_corpus: str,
             corpus_df_to_mallet(corpus_df_train, path_corpus)
         elif trainer == "ctm":
             path_corpus = pathlib.Path(path_corpus).parent.joinpath('corpus_train.parquet')
-            corpus_df_val.to_parquet(path_corpus)
+            corpus_df_train.to_parquet(path_corpus)
     else:
         # If no reference corpus is provided, we use the original corpus as the reference corpus (not the best option for coherence comparison, but it works)
         path_ref_corpus = path_corpus

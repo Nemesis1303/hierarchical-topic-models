899523fd67cad7a57d59f2a74dcf5d7b50c023b2
Lorena Calvo Bartolomé <100330717@alumnos.uc3m.es>
2023-08-22 09:57:35 +0200

    Modified rbo calculation.

--- experiments/variability/calculate_exclusivity.py
+++ experiments/variability/calculate_exclusivity.py
@@ -7,35 +7,36 @@ import pathlib
 
 # Add src to path and make imports
 sys.path.append('../..')
-from src.utils.misc import mallet_corpus_to_df
 from src.topicmodeler.src.topicmodeling.manageModels import TMmodel
+from src.utils.misc import mallet_corpus_to_df
 
 def recalculate_cohr(corpus_val, path_models, root_topics):
     root_topics = root_topics.split(',')
     for root_topic in root_topics:
-        path_models_tpc = pathlib.Path(path_models).joinpath(f"{root_topic}_tpc_root")
-        
+        path_models_tpc = pathlib.Path(
+            path_models).joinpath(f"{root_topic}_tpc_root")
+
         print(f"-- -- Recalculating coherence for {path_models_tpc}")
-        
+
         corpus_df = mallet_corpus_to_df(pathlib.Path(corpus_val))
         corpus_df['text'] = corpus_df['text'].apply(lambda x: x.split())
-        
+
         def get_cohrs(model_path):
             if np.load(model_path.joinpath("TMmodel").joinpath(
-                'new_topic_coherence.npy'), allow_pickle=True).tolist() is None:
+                    'new_topic_coherence.npy'), allow_pickle=True).tolist() is None:
                 tm = TMmodel(model_path.joinpath("TMmodel"))
                 cohr = tm.calculate_topic_coherence(
-                        metrics=["c_npmi"],
-                        reference_text=corpus_df.text.values.tolist(),
-                        aggregated=False,
-                    )
+                    metrics=["c_npmi"],
+                    reference_text=corpus_df.text.values.tolist(),
+                    aggregated=False,
+                )
                 if os.path.exists(model_path.joinpath("TMmodel").joinpath(
-                    'new_topic_coherence.npy').as_posix()):
+                        'new_topic_coherence.npy').as_posix()):
                     os.remove(model_path.joinpath("TMmodel").joinpath(
-                    'new_topic_coherence.npy').as_posix())
+                        'new_topic_coherence.npy').as_posix())
                 np.save(model_path.joinpath("TMmodel").joinpath(
                     'new_topic_coherence.npy'), cohr)
-        
+
         def get_rbo(model_path):
             tm = TMmodel(model_path.joinpath("TMmodel"))
             rbo = tm.calculate_rbo()
@@ -43,13 +44,14 @@ def recalculate_cohr(corpus_val, path_models, root_topics):
             np.save(model_path.joinpath("TMmodel").joinpath(
                     'rbo.npy'), rbo)
             return
-        
+
         for entry in path_models_tpc.iterdir():
             get_rbo(entry)
             for entry_ in entry.iterdir():
                 if entry_.joinpath('TMmodel/alphas.npy').is_file() and not entry_.as_posix().endswith("old"):
-                    get_rbo(entry_)            
-    
+                    get_rbo(entry_)
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--path_models', type=str,
@@ -59,13 +61,14 @@ def main():
                         default="/export/usuarios_ml4ds/lbartolome/Datasets/CORDIS/models_preproc/iter_0/corpus_val.txt",
                         help="Path to the validation training data.")
     parser.add_argument('--root_topics', type=str,
-                    default="5,10,20",
-                    help="Nr of topics for the root model.")
-    
+                        default="5,10,20",
+                        help="Nr of topics for the root model.")
+
     args = parser.parse_args()
     recalculate_cohr(corpus_val=args.path_val_corpus,
                      path_models=args.path_models,
                      root_topics=args.root_topics)
+
 
 if __name__ == "__main__":
     main()
